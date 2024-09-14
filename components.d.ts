952b74f04c7162b1cd00cd15e204973d1860871c
Jian Guo <guojian_k@qq.com>
2024-09-14 15:47:26 +0800

    feat: update

--- components.d.ts
+++ components.d.ts
@@ -8,12 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     DownloadButton: typeof import('./src/components/DownloadButton.vue')['default']
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElImage: typeof import('element-plus/es')['ElImage']
-    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
-    ElText: typeof import('element-plus/es')['ElText']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
@@ -23,7 +18,5 @@ declare module 'vue' {
     PageSection: typeof import('./src/components/PageSection.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
