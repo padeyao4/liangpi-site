b0f8fc79930033be4cb010139e454e87185f6477
Jian Guo <guojian_k@qq.com>
2024-09-14 14:20:46 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { faker } from '@faker-js/faker';
 import DownloadButton from '@/components/DownloadButton.vue';
 import PageSection from '@/components/PageSection.vue';
+import Detail from '@/assets/detail.png';
 </script>
 
 <template>
@@ -15,14 +16,14 @@ import PageSection from '@/components/PageSection.vue';
         <li class="flex items-center justify-center p-2 hover:bg-gray-200">
           <a href="#download">下载</a>
         </li>
-        <li class="flex items-center justify-center p-2 hover:bg-gray-200">
-          <a href="#contact">联系</a>
-        </li>
+        <!--        <li class="flex items-center justify-center p-2 hover:bg-gray-200">-->
+        <!--          <a href="#contact">联系</a>-->
+        <!--        </li>-->
       </ul>
     </nav>
   </header>
   <main class="flex flex-col">
-    <page-section v-for="_ in 3" :key="_" class="h-screen" id="about">
+    <!--    <page-section v-for="_ in 3" :key="_" class="h-screen" id="about">
       <div>
         {{ faker.lorem.paragraphs(5) }}
       </div>
@@ -34,11 +35,28 @@ import PageSection from '@/components/PageSection.vue';
           <div class="image-slot">Loading<span class="dot">...</span></div>
         </template>
       </el-image>
+    </page-section>-->
+    <page-section class="h-screen" id="about">
+      <div>
+        <div class="mb-8 ml-0 text-gray-500">
+          这是一个使用 Vue + Vite + Electron 开发的可视化的方式目标管理软件
+        </div>
+        <ul class="font-light text-gray-500" style="font-size: 15px; list-style-type: disc">
+          <li>使用带有时间轴的画布来显示任务，可以清晰地显示任务之间的关联关系。</li>
+          <li>任务可以发布在公共空间,任何人都可以通过关注来查看和参与任务。(待实现)</li>
+          <li>软件会每天筛选出当天需要执行的任务，显示在每日任务中。</li>
+        </ul>
+      </div>
+      <el-image :src="Detail" style="position: static !important; width: 50% ">
+        <template #placeholder>
+          <div class="image-slot">Loading<span class="dot">...</span></div>
+        </template>
+      </el-image>
     </page-section>
-    <page-section style="height: 800px" id="download">
+    <page-section class="h-screen" id="download">
       <div class="flex flex-col gap-4">
         <div class="flex flex-row items-center justify-center gap-4">
-          <h1>{{ faker.location.country() }}</h1>
+          <h1>Possible</h1>
           <download-button />
         </div>
         <div>
@@ -52,8 +70,8 @@ import PageSection from '@/components/PageSection.vue';
         </div>
       </div>
     </page-section>
-    <page-section class="h-screen" id="contact">
-      <h1>联系我们</h1>
-    </page-section>
+    <!--    <page-section class="h-screen" id="contact">-->
+    <!--      <h1>联系我们</h1>-->
+    <!--    </page-section>-->
   </main>
 </template>
--- src/components/PageSection.vue
+++ src/components/PageSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex items-center justify-center border-b border-dashed">
+  <section class="flex items-center justify-center border-b border-dashed border-gray-400">
     <div class="container flex flex-colo items-center justify-center gap-24 py-32">
       <slot></slot>
     </div>
